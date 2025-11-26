8b776f2b0794e1a0eec7900f0ae38a87a1c046ea
SsahilxydD <ysahil816@gmail.com>
2025-11-26 17:34:44 +0530

    UI CHANGES

--- src/components/ProductDomeGallery.jsx
+++ src/components/ProductDomeGallery.jsx
@@ -82,7 +82,7 @@ const ProductDomeGallery = () => {
           maxVerticalRotationDeg={5}
           dragSensitivity={20}
           enlargeTransitionMs={300}
-          segments={35}
+          segments={10}
           dragDampening={2}
           openedImageWidth="300px"
           openedImageHeight="400px"
