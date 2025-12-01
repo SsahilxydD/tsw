6fce6aa5e7bacf224973f37617a819a2b7838039
SsahilxydD <ysahil816@gmail.com>
2025-12-01 16:32:07 +0530

    Sourcemap Added

--- src/components/CartDrawer.jsx
+++ src/components/CartDrawer.jsx
@@ -161,7 +161,7 @@ const CartDrawer = () => {
                                     <button
                                         onClick={() => {
                                             setIsCartOpen(false);
-                                            navigate('/place-order');
+                                            navigate('/address');
                                         }}
                                         className="w-full py-3 bg-black text-white text-sm font-medium uppercase tracking-wider hover:bg-gray-800 transition-colors"
                                     >
